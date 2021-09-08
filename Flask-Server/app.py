215e0c52affb5988deec995ae829b6e11095d453
LoginDenied <adjovi1996@hotmail.com>
2021-09-09 06:20:42 +0800

    Completed add family member

--- Flask-Server/app.py
+++ Flask-Server/app.py
@@ -24,7 +24,35 @@ def createHousehold():
         conn.close()
         return jsonify({"HouseID" : houseID})
     except pymysql.err.IntegrityError:
+        # TODO: amend this error message
         return jsonify({"Error" : "HousingType must be either Landed/Condominium/HDB"}) 
+    except:
+        abort(400)
+
+@app.route("/add-member", methods=["POST"])
+def addMember():
+    json_data = request.json
+    if json_data == None or "HouseID" not in json_data or "Member" not in json_data:
+        return jsonify({"Error" : "HouseID and Member must be present in a json format"}) 
+    conn = pymysql.connect(
+        host=SQL_HOST,
+        db=SQL_DB,
+        user=SQL_USER,
+        password=SQL_PASSWORD
+    )
+    cursor = conn.cursor()
+    try:
+        member_data = json_data["Member"]
+        if "Spouse" in json_data["Member"]:
+            cursor.execute("INSERT INTO MemberLivesIn (HouseID, Name, Gender, MaritalStatus, Spouse, OccupationType, AnnualIncome, DOB) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (json_data["HouseID"], member_data["Name"], member_data["Gender"], member_data["MaritalStatus"], member_data["Spouse"], member_data["OccupationType"], member_data["AnnualIncome"], member_data["DOB"]))
+        else:
+            cursor.execute("INSERT INTO MemberLivesIn (HouseID, Name, Gender, MaritalStatus, OccupationType, AnnualIncome, DOB) VALUES (%s, %s, %s, %s, %s, %s, %s)", (json_data["HouseID"], member_data["Name"], member_data["Gender"], member_data["MaritalStatus"], member_data["OccupationType"], member_data["AnnualIncome"], member_data["DOB"]))
+        
+        conn.commit()
+        conn.close()
+        return jsonify({"HouseID" : json_data["HouseID"]})
+    except pymysql.err.IntegrityError:
+        return jsonify({"Error" : "Please check the allowed values"}) 
     except:
         abort(400)
 
